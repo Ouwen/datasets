0afbb38fa851c4286eb665de2b166232b3d2f5db
Ouwen Huang <ouwen.huang@duke.edu>
2020-03-20 01:55:36 +0000

    minor

--- tensorflow_datasets/image/duke_ultranet.py
+++ tensorflow_datasets/image/duke_ultranet.py
@@ -298,7 +298,7 @@ class DukeUltranet(tfds.core.BeamBasedBuilder):
         return [
             tfds.core.SplitGenerator(
                     name=tfds.Split.TRAIN,
-                    num_shards=len(_FILES)
+                    num_shards=len(_FILES),
                     gen_kwargs={
                         'files': _FILES
                     }
